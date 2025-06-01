4382a05ee0157fa87cbfa9e032e9adb71527b405
Jackson Silva <jacksonsilva_fsa@outlook.com>
2025-06-01 10:44:05 -0300

    Arquivos de configuraçao do Lightweight IP

--- ATIVIDADE-08.c
+++ ATIVIDADE-08.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
 
 
 
@@ -9,7 +9,5 @@ int main()
     stdio_init_all();
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
     }
 }
